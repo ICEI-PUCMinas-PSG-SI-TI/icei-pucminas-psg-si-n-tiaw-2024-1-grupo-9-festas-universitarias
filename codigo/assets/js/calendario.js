5eb146b8d6ab668a831b43fdc09e5626dd50625f
Rezende <arezendealmeida@gmail.com>
2024-05-01 19:49:12 -0300

    feat: full calendar

--- codigo/assets/js/calendario.js
+++ codigo/assets/js/calendario.js
@@ -1,1 +1,30 @@
-console.log("hello")
+//console.log("hello")
+
+//script de criação do calendario
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        
+        themeSystem: 'bootstrap',
+
+
+        eventContent: function(arg) {
+            return {
+                html: '<b>' + arg.timeText + '</b> ' + arg.event.title
+            };
+        },
+
+        
+        
+        
+
+        height: 'auto',
+        aspectRatio: 1.5,
+
+        
+        locale: 'pt-br'
+    });
+
+    calendar.render();
+});
