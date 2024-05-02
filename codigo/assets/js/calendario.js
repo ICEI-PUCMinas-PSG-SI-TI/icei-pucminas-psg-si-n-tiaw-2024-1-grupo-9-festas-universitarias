d624148f3b3ffb6fa7f898838c478fa343fd7524
Rezende <arezendealmeida@gmail.com>
2024-05-01 22:48:26 -0300

    feat: inserção de eventos e legenda

--- codigo/assets/js/calendario.js
+++ codigo/assets/js/calendario.js
@@ -1,28 +1,36 @@
-//console.log("hello")
-
-//script de criação do calendario
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
+    var eventos = [
+        {
+            title: "Calourada PUC",
+            start: moment().format("YYYY-MM-DD"), 
+            end:  moment().format("YYYY-MM-DD"), 
+            color: "#8A2BE2",
+            display: "block",
+        },
+        {
+            title: "Palestra Networking",
+            start: '2024-05-05', 
+            end: '2024-05-07', 
+            color: "#21a34f",
+            display: "block",
+        },
+    ]
+    
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        
+      
         themeSystem: 'bootstrap',
-
-
+        eventColor: 'green',
+        nextDayThreshold: '09:00:00',
         eventContent: function(arg) {
             return {
                 html: '<b>' + arg.timeText + '</b> ' + arg.event.title
             };
         },
-
-        
-        
-        
-
+        events: eventos,
         height: 'auto',
         aspectRatio: 1.5,
-
-        
         locale: 'pt-br'
     });
 
