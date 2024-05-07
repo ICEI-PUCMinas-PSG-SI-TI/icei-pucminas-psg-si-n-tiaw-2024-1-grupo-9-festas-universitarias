806df8a23d832948d42f66e67b911f028017f530
Rezende <arezendealmeida@gmail.com>
2024-05-06 21:28:39 -0300

    eventos cadastrados sendo exibidos

--- codigo/assets/js/calendario.js
+++ codigo/assets/js/calendario.js
@@ -1,49 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
+    var calendar;
 
-    var eventos = [
-        {
-            title: "Calourada PUC",
-            start: moment().format(), 
-            end:  moment().format(), 
-            color: "#8A2BE2",
-            display: "block",
-        },
-        {
-            title: "Palestra Networking",
-            start: '2024-05-05', 
-            end: '2024-05-07', 
-            color: "#21a34f",
-            display: "block",
-        },
-    ]
-    
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      
-        themeSystem: 'bootstrap',
-        eventColor: 'green',
-        nextDayThreshold: '09:00:00',
-        eventContent: function(arg) {
-            return {
-                html: '<b>' + arg.timeText + '</b> ' + arg.event.title
-            };
-        },
-        events: eventos,
-        height: 'auto',
-        aspectRatio: 1.5,
-        locale: 'pt-br'
-    });
+    buscarEventos()
+        .then(eventos => {
+            calendar = new FullCalendar.Calendar(calendarEl, {
+                themeSystem: 'bootstrap',
+                eventColor: 'green',
+                nextDayThreshold: '09:00:00',
+                eventContent: function(arg) {
+                    return {
+                        html: '<b>' + arg.timeText + '</b> ' + arg.event.title
+                    };
+                },
+                events: eventos,
+                height: 'auto',
+                aspectRatio: 1.5,
+                locale: 'pt-br'
+            });
 
-    calendar.render();
+            calendar.render();
+        })
+        .catch(error => {
+            console.error('Erro ao buscar eventos:', error);
+        });
 });
 
 $(document).ready(function() {
     $("#lastButton").click(function() {
       $("#modalNovoEvento").modal("show");
     });
-  });
+});
 
-  $('#preco').change(function(){
+$('#preco').change(function(){
     let valor = $(this).val().replace(/[^\d,]/g, '');
     
     let valorNumerico = parseFloat(valor.replace(',', '.'));
@@ -57,11 +46,43 @@ $(document).ready(function() {
         $(this).val("");
     }else{
         $(this).val(valorFormatado);
-
     }
-
 
     console.log(valor, valorFormatado);
 });
 
-
+function buscarEventos() {
+    return fetch("http://localhost:3000/eventos")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao fazer a requisição: ' + response.statusText);
+        }
+        return response.json(); 
+      })
+      .then(data => {
+        let eventos = []
+        
+        data.forEach(dado => {
+            let evento = ""
+            let color = ""
+            if(dado.tipo == "1"){
+                color = "#8A2BE2"
+            }else{
+                color = "#21a34f"
+            }
+            evento = {
+                "title": dado.nomeEvento,
+                "start": dado.dataInicio,
+                "end": dado.dataFim,
+                "color": color,
+                "display": "block"
+            }
+            eventos.push(evento)
+        });
+  
+        return eventos
+      })
+      .catch(error => {
+        console.error('Ocorreu um erro:', error);
+      });
+}
