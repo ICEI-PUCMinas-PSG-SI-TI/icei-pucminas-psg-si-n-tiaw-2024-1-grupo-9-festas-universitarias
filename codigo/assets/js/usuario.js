55c2080c83b45559653b065c96c9aa928a090d40
ThiagoVM11 <thiagovm016@gmail.com>
2024-06-12 00:09:55 -0300

    Inserido Requisito Edicao de Campos do Usuario - (name, email, ftPerfil, InstituicaoEnsino)

--- codigo/assets/js/usuario.js
+++ codigo/assets/js/usuario.js
@@ -5,9 +5,10 @@ $(document).ready(function () {
     try {
       const response = await axios.get('http://localhost:3000/logado');
       usuarioAtual = response.data[0];
-      console.log(usuarioAtual.fotoPerfil);
       $('#usuario').text(`${usuarioAtual.nome.primeiroNome} ${usuarioAtual.nome.ultimoNome}`);
       $('#UserLogado').attr('placeholder', `${usuarioAtual.nome.primeiroNome} ${usuarioAtual.nome.ultimoNome}`);
+      $('#EmailUser').attr('placeholder', `${usuarioAtual.email}`);
+      $('#NameUniversidade').attr('placeholder', `${usuarioAtual.nomeuniversidade}`);
       if (usuarioAtual.fotoPerfil === "") {
         $('#ftSaldacao').attr('src', 'https://sistemas.ft.unicamp.br/sgpg/imagens/sem_foto.png');
         $('#ftPerfil').attr('src', 'https://sistemas.ft.unicamp.br/sgpg/imagens/sem_foto.png');
@@ -26,33 +27,60 @@ $(document).ready(function () {
     $('#ftInput').click();
   });
 
-  $('#ftInput').on('change', function (event) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = function (e) {
-        const img = new Image();
-        img.onload = function () {
-          const canvas = document.createElement('canvas');
-          const ctx = canvas.getContext('2d');
-          canvas.width = img.width;
-          canvas.height = img.height;
-          ctx.drawImage(img, 0, 0);
-
-          const jpegDataUrl = canvas.toDataURL('image/jpeg'); // Converte para JPEG
-          $('#ftPerfil').attr('src', jpegDataUrl);
-          $('#ftSaldacao').attr('src', jpegDataUrl);
-          usuarioAtual.fotoPerfil = jpegDataUrl;
+  $('#ftInput').on('change', function (e) {
+    const arquivo = e.target.files[0];
+    if (arquivo) {
+        const leitor = new FileReader();
+        leitor.onload = function (e) {
+            const UrlBase64 = e.target.result;
+            $('#ftPerfil').attr('src', UrlBase64);
+            $('#ftSaldacao').attr('src', UrlBase64);
+            usuarioAtual.fotoPerfil = UrlBase64;
         };
-        img.src = e.target.result; // Define o src da imagem
-      };
-      reader.readAsDataURL(file); // Lê o arquivo como uma URL de dados
+        leitor.readAsDataURL(arquivo);
+    }
+});
+
+
+  
+$('#editUser').on('click', function () {
+    var isDisabled = $('#UserLogado').prop('disabled');
+    $('#UserLogado').prop('disabled', !isDisabled).focus();
+});
+$('#UserLogado').on('change', function (e) {
+  const nomeCompleto = e.target.value;
+  const nomes = nomeCompleto.split(' ');
+
+    if (nomes.length >= 2) {
+        usuarioAtual.nome.primeiroNome = nomes[0];
+        usuarioAtual.nome.ultimoNome = nomes[1];
+    } else {
+        usuarioAtual.nome.primeiroNome = nomes[0];
+        usuarioAtual.nome.ultimoNome = '';
     }
   });
 
-  $('#editUser').on('click', function () {
-    $('#UserLogado').prop('disabled', false).focus();
-  });
+$('#editEmail').on('click', function () {
+    var isDisabled = $('#EmailUser').prop('disabled');
+    $('#EmailUser').prop('disabled', !isDisabled).focus();
+});
+$('#EmailUser').on('change', function(e){
+  const Email = e.target.value;
+  if (Email.length != 0){
+    usuarioAtual.email = Email;
+  } else {usuarioAtual.email = usuarioAtual.email}
+})
+
+$('#editNameUni').on('click', function () {
+    var isDisabled = $('#NameUniversidade').prop('disabled');
+    $('#NameUniversidade').prop('disabled', !isDisabled).focus();
+});
+$('#NameUniversidade').on('change', function(e){
+  const NameUniversidade = e.target.value;
+  if (NameUniversidade.length != 0){
+    usuarioAtual.nomeuniversidade = NameUniversidade;
+  } else {usuarioAtual.nomeuniversidade = usuarioAtual.nomeuniversidade}
+})
 
   $('#btnSalvar').on('click', async function () {
     try {
